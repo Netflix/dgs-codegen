6def1f18376dd1ae08f1ad9611493ee2a5c9452e
Cole Turner <cole@netflix.com>
2023-06-23 13:20:55 -0700

    fix: generate input types when client api is generated

--- graphql-dgs-codegen-core/src/main/kotlin/com/netflix/graphql/dgs/codegen/CodeGen.kt
+++ graphql-dgs-codegen-core/src/main/kotlin/com/netflix/graphql/dgs/codegen/CodeGen.kt
@@ -63,9 +63,7 @@ class CodeGen(private val config: CodeGenConfig) {
     private val document = buildDocument()
     private val requiredTypeCollector = RequiredTypeCollector(
         document = document,
-        queries = config.includeQueries,
-        mutations = config.includeMutations,
-        subscriptions = config.includeSubscriptions
+        config = config
     )
 
     @Suppress("DuplicatedCode")
