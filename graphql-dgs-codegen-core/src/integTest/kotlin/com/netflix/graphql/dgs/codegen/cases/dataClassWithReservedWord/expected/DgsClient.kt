fabc1f2c6fb49bf5931cb92b54c191f3fb3e78fe
Matt Bossenbroek <mbossenbroek@netflix.com>
2023-11-27 10:45:16 -0800

    Fix escaping of reserved words in kotlin2 generated data types

---
+++ graphql-dgs-codegen-core/src/integTest/kotlin/com/netflix/graphql/dgs/codegen/cases/dataClassWithReservedWord/expected/DgsClient.kt
@@ -0,0 +1,3 @@
+package com.netflix.graphql.dgs.codegen.cases.dataClassWithReservedWord.expected
+
+public object DgsClient
---
+++ graphql-dgs-codegen-core/src/integTest/kotlin/com/netflix/graphql/dgs/codegen/cases/dataClassWithReservedWord/expected/client/SampleTypeProjection.kt
@@ -0,0 +1,11 @@
+package com.netflix.graphql.dgs.codegen.cases.dataClassWithReservedWord.expected.client
+
+import com.netflix.graphql.dgs.codegen.GraphQLProjection
+
+public class SampleTypeProjection : GraphQLProjection() {
+  public val `return`: SampleTypeProjection
+    get() {
+      field("return")
+      return this
+    }
+}
