aefe5fd8bf366a37cc2d403ba7d8d18537bcd0cb
Matt Bossenbroek <mbossenbroek@netflix.com>
2024-01-08 14:57:23 -0800

    Kotlin2: Update projection name to be derived from GQL name instead of kotlin name. This enables support for generic classes to be used in the type mapping.

--- graphql-dgs-codegen-core/src/integTest/kotlin/com/netflix/graphql/dgs/codegen/cases/dataClassWithMappedTypes/expected/types/Query.kt
+++ graphql-dgs-codegen-core/src/integTest/kotlin/com/netflix/graphql/dgs/codegen/cases/dataClassWithMappedTypes/expected/types/Query.kt
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.`annotation`.JsonProperty
 import com.fasterxml.jackson.`annotation`.JsonTypeInfo
 import com.fasterxml.jackson.databind.`annotation`.JsonDeserialize
 import com.fasterxml.jackson.databind.`annotation`.JsonPOJOBuilder
+import graphql.relay.SimpleListConnection
 import java.lang.IllegalStateException
 import kotlin.collections.List
 import kotlin.jvm.JvmName
@@ -13,18 +14,18 @@ import kotlin.jvm.JvmName
 @JsonDeserialize(builder = Query.Builder::class)
 public class Query(
   entity: () -> List<Entity?>? = entityDefault,
-  entityConnection: () -> EntityConnection? = entityConnectionDefault,
+  entityConnection: () -> SimpleListConnection<EntityEdge>? = entityConnectionDefault,
 ) {
   private val _entity: () -> List<Entity?>? = entity
 
-  private val _entityConnection: () -> EntityConnection? = entityConnection
+  private val _entityConnection: () -> SimpleListConnection<EntityEdge>? = entityConnection
 
   @get:JvmName("getEntity")
   public val entity: List<Entity?>?
     get() = _entity.invoke()
 
   @get:JvmName("getEntityConnection")
-  public val entityConnection: EntityConnection?
+  public val entityConnection: SimpleListConnection<EntityEdge>?
     get() = _entityConnection.invoke()
 
   public companion object {
@@ -32,7 +33,7 @@ public class Query(
         { throw IllegalStateException("Field `entity` was not requested") }
 
 
-    private val entityConnectionDefault: () -> EntityConnection? = 
+    private val entityConnectionDefault: () -> SimpleListConnection<EntityEdge>? = 
         { throw IllegalStateException("Field `entityConnection` was not requested") }
 
   }
@@ -42,7 +43,7 @@ public class Query(
   public class Builder {
     private var entity: () -> List<Entity?>? = entityDefault
 
-    private var entityConnection: () -> EntityConnection? = entityConnectionDefault
+    private var entityConnection: () -> SimpleListConnection<EntityEdge>? = entityConnectionDefault
 
     @JsonProperty("entity")
     public fun withEntity(entity: List<Entity?>?): Builder = this.apply {
@@ -50,7 +51,8 @@ public class Query(
     }
 
     @JsonProperty("entityConnection")
-    public fun withEntityConnection(entityConnection: EntityConnection?): Builder = this.apply {
+    public fun withEntityConnection(entityConnection: SimpleListConnection<EntityEdge>?): Builder =
+        this.apply {
       this.entityConnection = { entityConnection }
     }
 
