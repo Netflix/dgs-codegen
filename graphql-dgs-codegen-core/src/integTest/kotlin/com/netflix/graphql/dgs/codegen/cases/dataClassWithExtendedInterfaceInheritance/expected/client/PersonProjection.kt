78f6c92be3d66fdd9a39508d5632eaa2cd1754f4
Samuel Gagarin <66745577+Lavmee@users.noreply.github.com>
2024-10-04 21:18:40 +0300

    feat: [k2] add support for data types inherited from extended interface (#741)
    * feat: add support for data types inherited from extended interface
    
    * refactor: change expected integration test by graphql specification & rollback extensionTypes definition (because of graphql specification)
    
    graphql specification (interface extensions): https://spec.graphql.org/October2021/#sel-GAHbpBBBBKBUytV
    
    * style: lint check

---
+++ graphql-dgs-codegen-core/src/integTest/kotlin/com/netflix/graphql/dgs/codegen/cases/dataClassWithExtendedInterfaceInheritance/expected/client/PersonProjection.kt
@@ -0,0 +1,26 @@
+package com.netflix.graphql.dgs.codegen.cases.dataClassWithExtendedInterfaceInheritance.expected.client
+
+import com.netflix.graphql.dgs.client.codegen.InputValueSerializerInterface
+import com.netflix.graphql.dgs.codegen.GraphQLProjection
+
+public class PersonProjection(
+  inputValueSerializer: InputValueSerializerInterface? = null,
+) : GraphQLProjection(inputValueSerializer) {
+  public val firstname: PersonProjection
+    get() {
+      field("firstname")
+      return this
+    }
+
+  public val lastname: PersonProjection
+    get() {
+      field("lastname")
+      return this
+    }
+
+  public fun onEmployee(_projection: EmployeeProjection.() -> EmployeeProjection):
+      PersonProjection {
+    fragment("Employee", EmployeeProjection(), _projection)
+    return this
+  }
+}
---
+++ graphql-dgs-codegen-core/src/integTest/kotlin/com/netflix/graphql/dgs/codegen/cases/dataClassWithExtendedInterfaceInheritance/expected/types/Query.kt
@@ -0,0 +1,42 @@
+package com.netflix.graphql.dgs.codegen.cases.dataClassWithExtendedInterfaceInheritance.expected.types
+
+import com.fasterxml.jackson.`annotation`.JsonIgnoreProperties
+import com.fasterxml.jackson.`annotation`.JsonProperty
+import com.fasterxml.jackson.`annotation`.JsonTypeInfo
+import com.fasterxml.jackson.databind.`annotation`.JsonDeserialize
+import com.fasterxml.jackson.databind.`annotation`.JsonPOJOBuilder
+import java.lang.IllegalStateException
+import kotlin.collections.List
+import kotlin.jvm.JvmName
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NONE)
+@JsonDeserialize(builder = Query.Builder::class)
+public class Query(
+  people: () -> List<Person?>? = peopleDefault,
+) {
+  private val __people: () -> List<Person?>? = people
+
+  @get:JvmName("getPeople")
+  public val people: List<Person?>?
+    get() = __people.invoke()
+
+  public companion object {
+    private val peopleDefault: () -> List<Person?>? = 
+        { throw IllegalStateException("Field `people` was not requested") }
+  }
+
+  @JsonPOJOBuilder
+  @JsonIgnoreProperties("__typename")
+  public class Builder {
+    private var people: () -> List<Person?>? = peopleDefault
+
+    @JsonProperty("people")
+    public fun withPeople(people: List<Person?>?): Builder = this.apply {
+      this.people = { people }
+    }
+
+    public fun build(): Query = Query(
+      people = people,
+    )
+  }
+}
